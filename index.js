9fed4a4188352899a8db102f70ca75aa9cfe1ac5
FEDERICO CURRAO - GQue - DEVELOPER <68132577+fcurrao@users.noreply.github.com>
2023-08-16 22:44:10 -0300

    index

---
+++ index.js
@@ -0,0 +1,15 @@
+export const suma = (num1, num2) => {
+  return num1 + num2;
+};
+
+export const resta = (num1, num2) => {
+  return num1 - num2;
+};
+
+export const mult = (num1, num2) => {
+  return num1 * num2;
+};
+
+export const div = (num1, num2) => {
+  return num1 / num2;
+};
